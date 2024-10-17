e86cdb4202646878a3686bf4e5a1f15aed3193ea
diaa562 <diaa13662@gmail.com>
2024-10-17 18:47:46 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function sal(){
+var d = prompt('ما هو سعر الدولار')
+var s = prompt('what is yor sallry')
+
+var f = d*s;
+
+document.write('sallry before:' + s + 'LE' +'</br>' + "sallry after:"  + f + '$' )
+
+
+
+}
+sal()
